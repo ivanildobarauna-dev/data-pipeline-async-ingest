e34c5e79504a4d530a4003a9bd3cf02a0743531b
Ivanildo Barauna de Souza Junior <ivanildo.jnr@outlook.com>
2024-07-29 18:47:30 -0300

    feat: adjust pipeline configs

--- pipeline/config/common.py
+++ pipeline/config/common.py
@@ -5,10 +5,13 @@ import logging
 
 class Constants:
     GCP_PROJECT = "ivanildobarauna"
-    PUBSUB_SUBSCRIPTION = f"projects/{GCP_PROJECT}/subscriptions/gcp-streaming-pipeline-pull"
+    PUBSUB_SUBSCRIPTION = (
+        f"projects/{GCP_PROJECT}/subscriptions/gcp-streaming-pipeline-pull"
+    )
 
 
-def default_timestamp_str() -> str: return str(int(datetime.now().timestamp()))
+def default_timestamp_str() -> str:
+    return str(int(datetime.now().timestamp()))
 
 
 def default_timestamp_formated() -> str:
@@ -16,13 +19,14 @@ def default_timestamp_formated() -> str:
 
 
 class EnvSetup(Enum):
-    PROD = 0,
+    PROD = (0,)
     TEST = 1
 
 
 def logger():
     logging.basicConfig(
         level=logging.CRITICAL,
-        format='%(asctime)s - %(name)s - %(levelname)s - %(message)s')
+        format="%(asctime)s - %(name)s - %(levelname)s - %(message)s",
+    )
 
     return logging.getLogger()
